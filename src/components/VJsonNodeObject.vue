9cdc42c5aafa2aea8601a86de88c7abd4b9df5a6
Andy Holland <github@ahweb.co.uk>
2023-07-26 00:22:24 +0100

    Object nodes can now display validation warning for repeated key values.

--- src/components/VJsonNodeObject.vue
+++ src/components/VJsonNodeObject.vue
@@ -11,7 +11,8 @@
             @input="setKey(index, $event)"
         >
           <template #append>
-            <b>:</b>
+            <b v-if="node.unique">:</b>
+            <v-icon v-else color="red" small right>mdi-alert</v-icon>
           </template>
         </v-text-field>
 
@@ -60,103 +61,82 @@ export default {
   },
 
   data() {
+    const {value} = this;
+
     return {
-      lastOutput: this.value,
+      lastOutput: value,
+      nodes: this.getNodes(value),
     };
   },
 
   methods: {
     appendValue($event) {
       // console.log("v-json-node-object.appendValue", $event);
-      const length = this.nodes.length;
-
-      const key = "key" + length;
+      const key = "";
 
       this.nodes.push({
         key,
         path: `${this.path}."${key}"`,
         value: $event,
+        unique: undefined, // to be calculated in onKeyChange()
       });
+
+      this.onKeyChange(key);
 
       this.dispatchInput();
     },
 
     deleteIndex(index) {
       // console.log("v-json-node-object.deleteIndex", index);
-      this.nodes.splice(index, 1);
+      const [{key}] = this.nodes.splice(index, 1);
+
+      this.onKeyChange(key);
 
       this.dispatchInput();
+    },
+
+    onKeyChange(newKey) {
+      // console.log("v-json-node-object.onKeyChange", newKey);
+
+      const {nodes} = this;
+
+      const sameKey = [];
+
+      for (let i = 0; i < nodes.length; i++) {
+        const node = nodes[i];
+
+        if (node.key === newKey) {
+          sameKey.push(i);
+        }
+      }
+
+      const isUnique = sameKey.length < 2;
+
+      for (let s = 0; s < sameKey.length; s++) {
+        const node = nodes[sameKey[s]];
+
+        this.$set(node, "unique", isUnique);
+      }
     },
 
     dispatchInput() {
       const output = this.getOutput();
 
-      if (typeof output === "undefined") {
-        this.lastOutput = output;
-        return;
-      }
+      const {lastOutput, value} = this;
 
-      const old = this.lastOutput;
-      // console.log("v-json-node-object.dispatchInput", output, old);
-      if (output !== old && output !== this.value) {
-        // console.log("v-json-node-object.dispatchInput >> input");
+      // console.log("v-json-node-object.dispatchInput", output);
+
+      if (typeof output === "undefined") {
+        this.lastOutput = undefined;
+
+      } else if (output !== lastOutput && output !== value) {
         this.lastOutput = output;
+
         this.$emit("input", output);
       }
     },
 
-    getOutput() {
-      const objectMembers = [];
-
-      const uniqueKeys = [];
-
-      let isValid = true;
-
-      for (let i = 0; i < this.nodes.length; i++) {
-        let {key, value} = this.nodes[i];
-
-        key = String(key);
-
-        if (uniqueKeys.includes(key)) {
-          isValid = false;
-          // Access all array members, even if not valid
-        }
-
-        uniqueKeys.push(key);
-
-        objectMembers.push(JSON.stringify(key) + ":" + value)
-      }
-
-      const output = !isValid ? undefined : `{${objectMembers.join(",")}}`;
-
-      // console.log("v-json-node-object.getOutput", output);
-
-      return output;
-    },
-
-    setValue(index, $event) {
-      // console.log("v-json-node-object.setValue", index, $event);
-      this.nodes[index].value = String($event);
-
-      this.dispatchInput();
-    },
-
-    setKey(index, $event) {
-      // console.log("v-json-node-object.setKey", index, $event);
-      this.nodes[index].key = String($event);
-
-      this.dispatchInput();
-    },
-  },
-
-  computed: {
-    elevation() {
-      return this.path.replace(/[^.]/g, "").length;
-    },
-
-    decoded() {
-      const {value} = this;
-
+    getDecoded(value) {
       let object = null;
 
       try {
@@ -170,26 +150,91 @@ export default {
       return null;
     },
 
-    nodes() {
-      const {decoded, path} = this;
+    getNodes(value) {
+      const {path} = this;
 
-      if (decoded == null) return [];
+      const decoded = this.getDecoded(value);
 
-      return Object.keys(decoded).map(key => {
-        const keyPath = /^\w+$/.test(key) ? key : JSON.stringify(key).replace(/\./g, "\\u002e").replace(/\[/g, "\\u005b");
+      if (decoded != null) {
+        return Object.keys(decoded).map(key => {
+          const keyPath = /^\w+$/.test(key) ? key : JSON.stringify(key).replace(/\./g, "\\u002e").replace(/\[/g, "\\u005b");
 
-        return {
-          key,
-          path: `${path}.${keyPath}`,
-          value: JSON.stringify(decoded[key]),
-        };
-      });
+          return {
+            key,
+            path: `${path}.${keyPath}`,
+            value: JSON.stringify(decoded[key]),
+            unique: true,
+          };
+        });
+      } else {
+        return [];
+      }
+    },
+
+    getOutput() {
+      const {nodes} = this;
+
+      const objectMembers = [];
+
+      let isValid = true;
+
+      for (let i = 0; i < nodes.length; i++) {
+        const node = nodes[i];
+
+        let {key, value, unique} = node;
+
+        key = String(key);
+
+        if (!unique) {
+          isValid = false;
+        } else if (isValid) {
+          objectMembers.push(JSON.stringify(key) + ":" + value)
+        }
+      }
+
+      if (isValid) {
+        return `{${objectMembers.join(",")}}`;
+      } else {
+        return undefined;
+      }
+    },
+
+    onValueChange(value) {
+      this.lastOutput = value;
+
+      this.nodes = this.getNodes(value);
+    },
+
+    setValue(index, $event) {
+      // console.log("v-json-node-object.setValue", index, $event);
+      this.$set(this.nodes[index], "value", String($event));
+
+      this.dispatchInput();
+    },
+
+    setKey(index, $event) {
+      // console.log("v-json-node-object.setKey", index, $event);
+      const oldKey = this.nodes[index].key;
+
+      const key = this.$set(this.nodes[index], "key", String($event));
+
+      this.onKeyChange(oldKey);
+
+      if (oldKey !== key) this.onKeyChange(key);
+
+      this.dispatchInput();
+    },
+  },
+
+  computed: {
+    elevation() {
+      return this.path.replace(/[^.]/g, "").length;
     },
   },
 
   watch: {
     value(value) {
-      this.lastOutput = value;
+      this.onValueChange(value);
     },
   },
 }
